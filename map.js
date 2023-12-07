52bce849c36850824e0bef31936e510a8bb9d9cd
spikysum <spikysum@gmail.com>
2023-12-07 10:41:35 +0900

    12071041

--- map.js
+++ map.js
@@ -1,12 +1,62 @@
+let map, infoWindow;
+var start,end;
+
 function initMap() {
   var opts = {
     zoom: 15,
     center: new google.maps.LatLng(35.68944, 139.69167)
   };
   var map = new google.maps.Map(document.getElementById("map"), opts);
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  directionsRenderer.setMap(map);
 
   var restaurantTypes = document.getElementById("restaurantTypes");
-  add_marker(map)
+  add_marker(map);
+
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(locationButton);
+
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Current Location");
+          infoWindow.open(map);
+          map.setCenter(pos);
+          start = pos;
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+    
+  });
+
+  const routeButton = document.createElement("button");
+  routeButton.textContent = "Display Route";
+  routeButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(routeButton);
+  directionsRenderer.setMap(map);
+  routeButton.addEventListener("click", () => {
+    calcRoute(directionsService, directionsRenderer);
+  });
+  
+
 }
 
 function add_marker(map){
@@ -32,7 +82,6 @@ function add_marker(map){
   }
 }
 
-
 function add_event_to_marker(marker, infoWindow, index) {
     var item = RESTAURANT_list[index];
     var restaurantTypes = document.getElementById("restaurantTypes");
@@ -50,6 +99,10 @@ function add_event_to_marker(marker, infoWindow, index) {
         infoWindows_hide();
         item['infoWindow'].open(map, item['marker']);
         restaurantTypes.innerHTML = "<span>"+typesListStr+"</span>";
+
+        var lat = item["location"]["latitude"]
+        var lng = item["location"]["longitude"]
+        end = new google.maps.LatLng(lat,lng);
     });
 }
 
@@ -57,4 +110,27 @@ function infoWindows_hide() {
     for (var i = 0; i < RESTAURANT_list .length; i++) {
         RESTAURANT_list[i]['infoWindow'].close();
     }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function calcRoute(directionsService, directionsRenderer) {
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request)
+  .then((response) => {
+    directionsRenderer.setDirections(response);
+  })
+  .catch((e) => window.alert("Click Current Location and Restaurant Pin"));
 }
